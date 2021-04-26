5948c851ff6343ad1bfb863e743ae8d449dadbff
edevars <e.devars.dev@gmail.com>
2021-04-26 17:23:58 -0500

    :sparkles: Generating in auto UUID

--- scripts/db/models/article.py
+++ scripts/db/models/article.py
@@ -2,6 +2,7 @@ from sqlalchemy import Column, String
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from uuid import uuid4
 
 import logging
 logging.basicConfig(level=logging.INFO)
@@ -11,7 +12,7 @@ base = declarative_base()
 class Article(base):
     __tablename__ = 'news'
 
-    article_id = Column(UUID(as_uuid=True), primary_key=True)
+    article_id = Column(UUID(as_uuid=True), primary_key=True,default=uuid4)
     url = Column(String)
     title = Column(String)
     article_text = Column(String)
@@ -21,10 +22,12 @@ class Article(base):
         session = Session()
         base.metadata.create_all(db)
 
-        saved_article = Article(article_id=article['article_id'], 
-                                url=article['url'],
+        saved_article = Article(url=article['url'],
                                 title=article['title'], 
                                 article_text=article['article_text'])
+       
         session.add(saved_article)
+        session.flush()
+        logging.info(f"Article saved with UUID {saved_article.article_id}")
         session.commit()
-        logging.info(f"Article saved with UUID {article['article_id']}")
+        
